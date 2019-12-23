ef29ac8d5779548c65c7b9ed2fff8f27fafeba39
bianavic <bianavic@g mail.com>
2019-12-23 11:21:02 -0300

    stack and debugger

---
+++ java-pilha/src/fluxo.java
@@ -0,0 +1,23 @@
+// PILHA ou STACK => organizar a execução do código
+// ( ELA existe para executar algo e lembrar o que ainda precisa ser executado)
+public class fluxo {
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Fim do metodo2");
+    }
+}
