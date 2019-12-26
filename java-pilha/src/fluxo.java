f9f625b5a65d1ecd02be6ad34e9f79520a8fa9fa
bianavic <bianavic@gmail.com>
2019-12-26 13:58:22 -0300

    try catch

--- java-pilha/src/fluxo.java
+++ java-pilha/src/fluxo.java
@@ -9,7 +9,12 @@ public class fluxo {
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        metodo2();
+        // usando try catch dentro metodo2
+        try {
+            metodo2();
+        } catch(ArithmeticException ex) {
+            System.out.println("ArithmeticException");
+        }
         System.out.println("Fim do metodo1");
     }
 
@@ -18,18 +23,7 @@ public class fluxo {
         for (int i = 1; i <= 5; i++) { // execucao de lacos
             System.out.println(i);
 
-            // lidar com excecoes
-            try {
                 int a = i / 0;
-            } catch(ArithmeticException ex) { // pegar a excecao e executar o codigo entre {}
-                System.out.println("ArithmeticException");
-            }
-
-            // excecoes mudam o fluxo (é uma bomba que cai em cima da pilha)
-            // entao ele chama se naquele metodo consegue executar o ArithmeticException?
-            // se nao existe, ele joga fora po metodo, mesmo sem te-lo finalizado.
-            // eliminando 1 a 1, a bomba cai no console. Onde visualizamos o rastro da bomba.
-            // int a = i / 0;
         }
         System.out.println("Fim do metodo2");
     }
