dfd299ab3b10bbff6a3e0bbb4439eb9b3d96bc55
bianavic <bianavic@gmail.com>
2019-12-26 14:26:02 -0300

    catch variations

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -1,20 +1,23 @@
+package src;
+
 // PILHA ou STACK => organizar a execução do código
 // ( ELA existe para executar algo e lembrar o que ainda precisa ser executado)
-public class fluxo {
+public class Fluxo {
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+            metodo1();
+        } catch(ArithmeticException | NullPointerException ex) { // pegamos a excecao
+            String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+            ex.printStackTrace(); // imprimir o rastro
+        }
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        // usando try catch dentro metodo2
-        try {
             metodo2();
-        } catch(ArithmeticException ex) {
-            System.out.println("ArithmeticException");
-        }
         System.out.println("Fim do metodo1");
     }
 
@@ -22,8 +25,9 @@ public class fluxo {
         System.out.println("Ini do metodo2");
         for (int i = 1; i <= 5; i++) { // execucao de lacos
             System.out.println(i);
-
-                int a = i / 0;
+            // int a = i / 0; // qdo comentado aparece apenas o NullPointerException
+                Conta c = null;
+                c.deposita();
         }
         System.out.println("Fim do metodo2");
     }
